da93017c3477ac8f9d6f6d3bc3d45042705bb4f6
Gustavo Jose Marcano <gustavexx@gmail.com>
2022-05-17 09:48:49 -0400

    testing AWS Amplify

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React in less than a day
+          Testing the AWS Amplify service
         </a>
       </header>
     </div>
